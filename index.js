6541b0da00f1809dbd882425e719735a45ead15e
niveus310 <niveus310@users.noreply.github.com>
2015-06-07 22:34:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-var L=100;
-var T=100;
 $(document).keydown(function(k){
     switch (k.which)
 	{
@@ -7,28 +5,21 @@ $(document).keydown(function(k){
         $(".box").finish().animate({
             left: "-=50"
         });
-		L-=50;
-		localStorage.setItem("boxLeft",L+"px")
+		break;
     case 38:  
         $(".box").finish().animate({
             top: "-=50"
         });
-		T-=50;
-		localStorage.setItem("boxTop",T+"px")
         break;
     case 39:    
         $(".box").finish().animate({
             left: "+=50"
         });
-		L+=50;
-		localStorage.setItem("boxLeft",L+"px")
         break;
     case 40:    
         $(".box").finish().animate({
             top: "+=50"
         }); 
-		T+=50;
-		localStorage.setItem("boxTop",T+"px")
 		break;
     }
 });

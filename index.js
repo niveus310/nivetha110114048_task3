d6471a6a9427c2bf61f84009d77522b7fd35d64a
niveus310 <niveus310@users.noreply.github.com>
2015-06-08 01:21:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,29 @@ $(document).keydown(function(k){
             top: "+=50"
         }); 
 		break;
-    }
+		}
+sessionStorage.setItem("boxTop",get_y());
+sessionStorage.setItem("boxLeft",get_x());
+		});
+
+
+$(window).load(function() {
+	var L = sessionStorage.getItem("boxLeft");
+	var T = sessionStorage.getItem("boxTop");
+	document.getElementById("box").style.left= L+"px";
+	document.getElementById("box").style.top = T+"px";
 });
 
+function get_x(){
+var element = document.getElementById("box");
+var position = element.getBoundingClientRect();
+var x = position.left;
+return x;
+}
 
+function get_y(){
+var element = document.getElementById("box");
+var position = element.getBoundingClientRect();
+var y = position.top;
+return y;
+}
